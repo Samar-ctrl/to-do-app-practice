c431a72da83270c7b24315ddf5d4189563f16951
Samar-ctrl <63056898+Samar-ctrl@users.noreply.github.com>
2021-11-04 23:31:41 +0530

    todoList app rough idea

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { React, useState } from "react";
 import "./styles.css";
-
+import Task from "./Task";
 export default function App() {
+  const [entry, setEntry] = useState("");
+  const [taskList, setList] = useState([]);
+  const inputHandler = (e) => {
+    // console.log(e.target.value);
+    setEntry(e.target.value);
+  };
+  const clickHandler = () => {
+    setList([...taskList, { item: entry, key: Date.now() }]);
+    console.log(taskList);
+    setEntry("");
+  };
   return (
     <div className="App">
       <header className="App-header">
         <div className="wrapper">
           <div className="input-wrapper">
-            <input />
-            <button>+</button>
+            <input
+              placeholder="Add an item"
+              value={entry}
+              onChange={inputHandler}
+            />
+            <button onClick={clickHandler}>+</button>
           </div>
+          <Task taskList={taskList} />
         </div>
       </header>
     </div>
---
+++ src/Task.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Task.css";
+export default function Task({ item, key }) {
+  console.log(key);
+  return (
+    <div className="item">
+      <p>HELLO </p>
+    </div>
+  );
+}
