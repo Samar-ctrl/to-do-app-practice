95b60db28559f37877c13bf68a9b8599199641f7
Samar-ctrl <63056898+Samar-ctrl@users.noreply.github.com>
2021-11-04 11:41:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="wrapper">
+          <div className="input-wrapper">
+            <input />
+            <button>+</button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
